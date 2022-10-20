c9093366dbe1ea2fd22af92890bebea0f23948f5
Deidaraylc <116264735+Deidaraylc@users.noreply.github.com>
2022-10-20 22:14:39 +0800

    Create SHVFS_P101_GD08_Deidara_C04.cs

---
+++ Classes/C04/SHVFS_P101_GD08_Deidara_C04.cs
@@ -0,0 +1,88 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace SHVFS_P101_GD08_Jerry
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var arithmetOpertorMultiply = 0;
+            //arithmetOpertorMultiply *= 10;
+            //Console.WriteLine(arithmetOpertorMultiply);
+            //Console.ReadLine();
+            //var arithmetOpertorMultiply = 4;
+            //arithmetOpertorMultiply++;
+            //Console.WriteLine(arithmetOpertorMultiply);
+            //Console.ReadLine();
+            //for (var i = 0; i < 10; i++) ;
+            //Console.WriteLine("This is BEFORE our FOR LOOP!");
+            //for (var I = 0; I < 10; I++) ;
+            {
+                //Console.WriteLine("Thsi is THE FIRST LINE in our FOR LOOP");
+                //Console.WriteLine($"The value of i is:{i}");
+
+
+            }
+            //Console.WriteLine("This is AFTER OUR FOR LOOP!");
+            //Console.ReadLine();
+           //ar input = "";
+
+           //hile (input != "Quit")
+            {
+               //onsole.Clear();
+               //onsole.WriteLine($"Enter \"Quit\" to win the game!");
+                //put = Console.ReadLine();
+
+            }
+
+           //onsole.WriteLine("CONGRATS! YOU WON!");
+          //Console.ReadLine();
+         // int i;
+          //Console.WriteLine("Enter 1 for options\n" +
+                           // "       2 to quit\n" +
+                           // "       3  to start game");
+         // do
+            {
+                //  Console.WriteLine("Plase make a selection! haha!");
+                // i = Convert.ToInt32(Console.ReadLine());
+            }//while (i < 1 || i > 3);
+         // Console.WriteLine("Exc ellent choice!");
+         // Console.ReadLine();
+         // for (var i = 0; i < 7; i++) ;
+            {
+              //for (var j = 0; j < 10; j++) ;
+                {
+                 // Console.WriteLine($"1 is: {i}, and j is: {j}");
+                }
+
+            }
+
+        //  Console.ReadLine();
+
+
+         // var scores = new int[5];
+         // scores[0] = 7;
+        //  scores[1] = 24;
+        //  scores[2] = 2;
+         // scores[3] = 9;
+        //  scores[4] = 1;
+         // for (var i = 0; i < scores.Length; i++) ;
+            {
+         //     Console.WriteLine($"The length of scores is: {scores.Length} | The index is: {i} | The value is: {scores[i]}");
+
+
+            }
+            //  Console.ReadLine();
+           
+
+
+
+
+
+
+
+
+        }
+    }
+}
